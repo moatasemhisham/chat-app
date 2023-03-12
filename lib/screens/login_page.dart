485ab7dea92d0b25590821e5c82ce5a6e90e7f32
moatasem hisham <moatasem.hisham5@gmial.com>
2023-03-12 06:34:50 +0200

    Using Bloc on a chat app from Tharwat samy's udemy course.

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -38,113 +38,115 @@ class LoginPage extends StatelessWidget {
 
         // TODO: implement listener
       },
-      builder:(context,state) => ModalProgressHUD(
-        inAsyncCall: isLoading,
-        child: Scaffold(
-          backgroundColor: kPrimaryColor,
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Form(
-              key: formKey,
-              child: ListView(
-                children: [
-                  SizedBox(
-                    height: 75,
-                  ),
-                  Image.asset(
-                    'assets/images/scholar.png',
-                    height: 100,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+      builder: (context, state) =>
+          ModalProgressHUD(
+            inAsyncCall: isLoading,
+            child: Scaffold(
+              backgroundColor: kPrimaryColor,
+              body: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Form(
+                  key: formKey,
+                  child: ListView(
                     children: [
-                      Text(
-                        'Scholar Chat',
-                        style: TextStyle(
-                          fontSize: 32,
-                          color: Colors.white,
-                          fontFamily: 'pacifico',
-                        ),
+                      SizedBox(
+                        height: 75,
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 75,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'LOGIN',
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.white,
-                        ),
+                      Image.asset(
+                        'assets/images/scholar.png',
+                        height: 100,
                       ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  CustomFormTextField(
-                    onChanged: (data) {
-                      email = data;
-                    },
-                    hintText: 'Email',
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  CustomFormTextField(
-                    obscureText: true,
-                    onChanged: (data) {
-                      password = data;
-                    },
-                    hintText: 'Password',
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  CustomButon(
-                      text: 'login',
-                      onTap: () async {
-                        if (formKey.currentState!.validate()) {
-                          BlocProvider.of<LoginCubit>(context)
-                              .loginUser(email: email!, password: password!);
-                        }else{
-                          // todo: handle thongs.
-                        }
-                      },),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'dont\'t have an account?',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, RegisterPage.id);
-                        },
-                        child: Text(
-                          '  Register',
-                          style: TextStyle(
-                            color: Color(0xffC7EDE6),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Scholar Chat',
+                            style: TextStyle(
+                              fontSize: 32,
+                              color: Colors.white,
+                              fontFamily: 'pacifico',
+                            ),
                           ),
-                        ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 75,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomFormTextField(
+                        onChanged: (data) {
+                          email = data;
+                        },
+                        hintText: 'Email',
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CustomFormTextField(
+                        obscureText: true,
+                        onChanged: (data) {
+                          password = data;
+                        },
+                        hintText: 'Password',
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CustomButon(
+                        text: 'login',
+
+                        onTap: () async {
+                          if (formKey.currentState!.validate()) {
+                            BlocProvider.of<LoginCubit>(context)
+                                .loginUser(email: email!, password: password!);
+                          } else {
+                            // todo: handle thongs.
+                          }
+                        },),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'dont\'t have an account?',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(context, RegisterPage.id);
+                            },
+                            child: Text(
+                              '  Register',
+                              style: TextStyle(
+                                color: Color(0xffC7EDE6),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
-      ),
     );
   }
 }
